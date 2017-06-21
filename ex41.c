f24be65399874988529726267dd7194eaddc3326
redperov <redperov@gmail>
2017-06-21 23:27:55 +0300

    Added thread pool

--- ex41.c
+++ ex41.c
@@ -1,3 +1,10 @@
+/******************************************
+* Student name: TODO add
+* Student ID: TODO add
+* Course Exercise Group: 05
+* Exercise name: ex4
+******************************************/
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -8,51 +15,51 @@
 #include <unistd.h>
 #include <signal.h>
 #include <memory.h>
+#include <ctype.h>
 
-/******************************************
-* Student name: TODO add
-* Student ID: TODO add
-* Course Exercise Group: 05
-* Exercise name: ex4
-******************************************/
-
-#define ARR_SIZE 5
 #define SHM_SIZE 4096
 
-int count_internal;
+/**
+ * function name: WriteMessage.
+ * The input: message to write.
+ * The output: void.
+ * The function operation: prints the message to the screen.
+*/
+void WriteMessage(char *message);
+
+/**
+ * function name: GetUserCommand.
+ * The input: void.
+ * The output: user command.
+ * The function operation: Reads command from user.
+*/
+char GetUserCommand();
+
+int HandleUserCommand(char command, char *data);
 
 int main(){
 
-    int file;
-    int key;
+    key_t key;
     int shmid;
+    int stop;
+    char command;
     char *data;
-
-
-    file = open("318810637.txt", O_CREAT, 0777);
-
-    //Check if open succeeded.
-    if(file < 0){
-
-        perror("Error: open failed.");
-        exit(1);
-    }
 
     //Create key.
     key = ftok("318810637.txt", 'A');
 
     //Check if ftok succeeded.
-    if(key < 0){
+    if (key < 0) {
 
-        perror("Error: ftok failed.");
+        perror("Error: ftok failed.\n");
         exit(1);
     }
 
-    //Create shared memory.
-    shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
+    //Get shared memory.
+    shmid = shmget(key, SHM_SIZE, 0666);
 
     //Check if shmget succeeded.
-    if(shmid < 0){
+    if (shmid < 0) {
 
         perror("Error: shmget failed.\n");
         exit(1);
@@ -62,16 +69,71 @@ int main(){
     data = shmat(shmid, NULL, 0);
 
     //Check if shmat succeeded.
-    if(data == (char*)(-1)){
+    if (data == (char *) -1) {
 
         perror("Error: shmat failed.\n");
         exit(1);
     }
 
+    stop = 0;
 
-    //Create threads array.
-    pthread_t threads[ARR_SIZE];
-    //TODO create job queue
-    count_internal = 0;
+    while(!stop){
+
+        WriteMessage("Please enter request code\n");
+
+        command = GetUserCommand();
+        stop = HandleUserCommand(command, data);
+    }
+
+
 }
 
+void WriteMessage(char *message){
+
+    int writeResult;
+
+    writeResult = write(1, message, strlen(message));
+
+    //Check if write succeeded.
+    if (writeResult < 0) {
+
+        perror("Error: write failed.\n");
+        exit(1);
+    }
+}
+
+char GetUserCommand(){
+
+    int readResult;
+    char command;
+
+    readResult = read(0, &command, 1);
+
+    //Check if read succeeded.
+    if (readResult < 0) {
+
+        perror("Error: read failed.\n");
+        exit(1);
+    }
+
+    command = tolower(command);
+
+    return command;
+}
+
+int HandleUserCommand(char command, char *data){
+
+    if(command == 'i'){
+
+        //TODO clean memory and exit
+        return 1;
+    }
+    else{
+
+        //TODO make sure the server finished reading
+        data[0] = command;
+
+        return 0;
+    }
+
+}
--- ex42.c
+++ ex42.c
@@ -5,6 +5,240 @@
 * Exercise name: ex4
 ******************************************/
 
-int main(){
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <memory.h>
+#include <time.h>
+
+
+
+#define ARR_SIZE 5
+#define SHM_SIZE 4096
+
+int count_internal;
+
+typedef struct node {
+
+    char        data;
+    struct node *next;
+}   Node;
+
+typedef struct {
+
+    Node *head;
+    Node *rear;
+}   Queue;
+
+void Display(Queue *queue);
+
+Queue *InitializeQueue();
+
+Queue *Enqueue(Queue *queue, char data);
+
+Queue *Dequeue(Queue *queue);
+
+char Top(Queue *queue);
+
+int IsEmpty(Queue *queue);
+
+int GenerateRandomValue();
+
+int main() {
+
+    key_t key;
+    int   file;
+    int   stop;
+    int   shmid;
+    char  *data;
+    char command;
+
+    file = open("318810637.txt", O_CREAT, 0777);
+
+    //Check if open succeeded.
+    if (file < 0) {
+
+        perror("Error: open failed.");
+        exit(1);
+    }
+
+    //Create key.
+    key = ftok("318810637.txt", 'A');
+
+    //Check if ftok succeeded.
+    if (key < 0) {
+
+        perror("Error: ftok failed.");
+        exit(1);
+    }
+
+    //Create shared memory.
+    shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
+
+    //Check if shmget succeeded.
+    if (shmid < 0) {
+
+        perror("Error: shmget failed.\n");
+        exit(1);
+    }
+
+    //Attach to shared memory.
+    data = shmat(shmid, NULL, 0);
+
+    //Check if shmat succeeded.
+    if (data == (char *) (-1)) {
+
+        perror("Error: shmat failed.\n");
+        exit(1);
+    }
+
+
+    //Create threads array.
+    pthread_t threads[ARR_SIZE];
+
+    //Create jobs queue.
+    Queue *jobsQueue;
+    jobsQueue = InitializeQueue();
+
+    //Initialize global counter.
+    count_internal = 0;
+
+    stop = 0;
+
+    while(!stop){
+
+        //Read command from shared memory.
+        command = data[0];
+
+        stop = HandleCommand(command, threads, jobsQueue, data);
+    }
 
 }
+
+int HandleCommand(char command, pthread_t *threads, Queue *jobsQueue, char *data){
+
+    if(command <= 'f'){
+
+        //Add job command to jobs queue.
+        Enqueue(jobsQueue, command);
+    }
+    else if(command == 'g'){
+
+    }
+    else{
+
+    }
+}
+
+Queue *InitializeQueue() {
+
+    Queue *queue = (Queue *) malloc(sizeof(Queue));
+    queue->rear = NULL;
+    queue->head = NULL;
+
+    return queue;
+}
+
+char Top(Queue *queue) {
+
+    return queue->head->data;
+}
+
+int IsEmpty(Queue *queue) {
+
+    return queue->head == NULL;
+}
+
+Queue *Enqueue(Queue *queue, char data) {
+
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (queue->rear == NULL) {
+
+        queue->rear = newNode;
+        queue->head = queue->rear;
+    } else {
+
+        Node *temp = queue->rear;
+        newNode->next = temp;
+        queue->rear   = newNode;
+    }
+
+    return queue;
+}
+
+Queue *Dequeue(Queue *queue) {
+
+    if (queue->head == NULL) {
+
+        return queue;
+    }
+
+    Node *temp = queue->rear;
+
+    while (temp->next != queue->head) {
+
+        temp = temp->next;
+    }
+
+    free(queue->head);
+    temp->next  = NULL;
+    queue->head = temp;
+
+    return queue;
+}
+
+void Display(Queue *queue) {
+
+    Node *temp = queue->rear;
+
+    while (temp != NULL) {
+
+        printf("%c -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+}
+
+void *ThreadFunction(void *arg){
+
+    char command = *((char*)arg);
+
+    if(command == 'f'){
+        //TODO
+    }
+    else{
+
+        int x = GenerateRandomValue();
+        int addToCounter = command - '0';
+        struct timespec ts = {0, x };
+
+        //Sleep.
+        nanosleep (&ts, NULL);
+
+        //Add to global counter.
+        count_internal += addToCounter;
+
+    }
+}
+
+int GenerateRandomValue(){
+
+    int randomVal;
+
+    srand(time(NULL));
+
+    //Generate random value.
+    randomVal = (rand() % 90) + 10;
+
+    return randomVal;
+}
+
